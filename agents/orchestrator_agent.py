f312058bfe44a95b6b89a6427a0accea6344d94d
Blagden1786 <georgeablagden@gmail.com>
2025-11-07 16:14:44 +0000

    Orchestrator runs triage agent

--- agents/orchestrator_agent.py
+++ agents/orchestrator_agent.py
@@ -1,15 +1,15 @@
 import re
 from agents.generic_agent import GenericAgent
+from agents.triage_agent import TriageAgent, SavingsTriageAgent
 # The triage agent will ask questions until it understands the ask, it will then generate a prompt for the other agent
 
 ORCHESTRATOR_PROMPT = """You are an orchestrator agent that's is to find out what the user is enquiring about and then hand over to the relevant agent. You will ask the user a series of questions to understand their needs. Once you have enough information, you will return python code that runs the correct agent. Make sure that your first message explains what you can do and asks the user what they need help with. You do not need to go into any more depth once you understand what agent you need to hand off to.
 
 You have access to the following agents:
-1. Savings Triage: Finds the best savings account for the user. Run with savings_triage
-2. Credit Triage: Finds the best credit card for the user. Run with credit_triage
+1. SavingsTriageAgent: Finds the best savings account for the user. Run with SavingsTriageAgent()
 
 When calling an agent, use exactly the following format:
-HANDOFF: <agent_name>
+HANDOFF: <agent_name>()
 
 For example, to call the savings agent, you would write:
 HANDOFF: savings_agent
@@ -25,7 +25,7 @@ class Orchestrator(GenericAgent):
 
         self.current_agent = self.run_agent
 
-        self.triage_agents_to_handoff = ['savings_triage', 'credit_triage']
+        self.triage_agents_to_handoff = ['SavingsTriageAgent()']
 
     def run_agent(self):
         while True:
@@ -34,7 +34,7 @@ class Orchestrator(GenericAgent):
 
             if response != None:
                 #TODO: Change Regex
-                next_question=re.search(r"(savings_triage)|(credit_triage)", response)
+                next_question=re.search(r"(SavingsTriageAgent\(\))", response)
 
                 # No tool is called then the agent has sufficient info so it has produced the final prompt to be used by the savings agent
                 if next_question != None:
@@ -42,7 +42,7 @@ class Orchestrator(GenericAgent):
 
                     # Move to next agent
                     if next_question.group() in self.triage_agents_to_handoff:
-                        self.current_agent = lambda : self.run_triage_agent(next_question.group())
+                        self.current_agent = lambda : self.run_triage_agent(eval(next_question.group()))
                         return
                 print(response)
                 answer = input("ANSWER: ")
@@ -52,5 +52,6 @@ class Orchestrator(GenericAgent):
             else:
                 return "ERROR: LLM failed to produce answer"
 
-    def run_triage_agent(self, type):
-        print("Triage time!!!!")
+    def run_triage_agent(self, agent:TriageAgent):
+        agent.run_agent()
+        return "Done"
